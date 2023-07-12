27749bfca14d2ad5e86793d3a0f8bcf3c931ee65
khakisage <jongjin201@gmail.com>
2023-07-12 23:59:59 +0900

    Chore: 라우터 용 임시 페이지

---
+++ src/pages/ArticleList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ArticleList(): JSX.Element {
+  return <div>ArticleList</div>;
+}
---
+++ src/pages/Main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Main() {
+  return (
+    <div className="hero min-h-screen bg-second">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl text-fourth font-bold">살까? 말까?</h1>
+          <p className="py-6 text-fourth">다른 사람들의 생각은 어떨까요? 고민될 땐, 물어봐요!</p>
+          <button className="btn bg-third text-fourth hover:bg-third2 border-none">
+            <Link to="/list">살펴보기</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
