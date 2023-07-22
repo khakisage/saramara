bbbfee9b9859910d5621c1358de94fa2e51ca1e3
khakisage <jongjin201@gmail.com>
2023-07-22 13:03:00 +0900

    Chore: firebase DB 설정

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,6 +1,7 @@
 import "firebase/compat/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_APIKEY,
@@ -13,6 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-// const db = firebase.firestore();
+const db = getFirestore(app);
 
-export { auth };
+export { auth, db };
