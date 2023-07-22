bd8854873e34bbbde278a4011df5ecda8d7de360
khakisage <jongjin201@gmail.com>
2023-07-22 13:04:16 +0900

    Feat: 에러 페이지 구현

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,3 +1,23 @@
+import { useRecoilState } from "recoil";
+import { loginState } from "../store/atom";
+
 export default function Error() {
-  return <div>Error</div>;
+  const isLogin = useRecoilState(loginState);
+  return (
+    <>
+      {isLogin ? (
+        <div className="hero min-h-screen bg-second">
+          <div className="hero-content flex-col">
+            <div className="text-left">로그인이 필요합니다.</div>
+          </div>
+        </div>
+      ) : (
+        <div className="hero min-h-screen bg-second">
+          <div className="hero-content flex-col">
+            <div className="text-left">잘못된 접근입니다.</div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
