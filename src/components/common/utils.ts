ed643ac244f0be0c3be5c24d76f96b4171312e57
khakisage <jongjin201@gmail.com>
2023-07-24 19:09:02 +0900

    Chore: util 함수 추가 - firestore 에서 articles collection 호출

--- src/components/common/utils.ts
+++ src/components/common/utils.ts
@@ -1,4 +1,6 @@
+import { collection, getDocs } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import { db } from "../../firebase-config";
 
 export const MovePage = ({ url }: { url: string }) => {
   const navigate = useNavigate();
@@ -6,4 +8,23 @@ export const MovePage = ({ url }: { url: string }) => {
     navigate(url);
   };
   return movePage;
+};
+
+type Article = {
+  id: string;
+  title: string;
+  content: string;
+  category: string;
+  price: number;
+  image: string | ArrayBuffer | null;
+  comments: any[];
+};
+
+export const fetchArticles = async () => {
+  const querySnapshot = await getDocs(collection(db, "articles"));
+  const articles: Article[] = [];
+  querySnapshot.forEach((doc) => {
+    articles.push({ id: doc.id, ...(doc.data() as Article) });
+  });
+  return articles;
 };
