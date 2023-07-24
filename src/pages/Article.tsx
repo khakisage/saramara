60318ed387414f85273f1c6045776bdf5c69767c
khakisage <jongjin201@gmail.com>
2023-07-24 19:13:10 +0900

    Feat: 게시글 상세 페이지_1 - url의 params로 게시글 상세 내용 조회 - api 호출 지연 시 빈 내용이 노출됨을 방지하기 위해 Loading 표시

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,16 +1,40 @@
-export default function Article() {
+import { useParams } from "react-router";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { articleListState, articleSpecState } from "../store/atom";
+import DOMPurify from "dompurify";
+import { useEffect, useState } from "react";
+import { fetchArticles } from "../components/common/utils";
+import Loading from "../components/common/Loading";
+
+export default function Article(): JSX.Element {
+  const { articleId } = useParams<{ articleId: string }>();
+  const setArticleList = useSetRecoilState(articleListState);
+  const [articleSpec, setArticleSpec] = useRecoilState(articleSpecState);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchArticles().then((fetchedArticles) => {
+      setArticleList(fetchedArticles);
+      // articleSpecState에 articleList의 length가 0일 때를 대비하여 articleSpecState의 type에 undefined도 추가
+      const matchedArticle = fetchedArticles.find((article) => article.id === articleId);
+      console.log("matchedArticle", matchedArticle);
+      setArticleSpec(matchedArticle || undefined);
+
+      setIsLoading(false);
+    });
+  }, [articleId, setArticleList, setArticleSpec]);
+
   return (
     <>
+      <Loading isLoading={isLoading} />
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+          <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={articleSpec?.image} />
           <div className="text-center lg:w-2/3 w-full">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
-            <p className="mb-8 leading-relaxed">
-              Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps
-              hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af
-              fingerstache pitchfork.
-            </p>
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{articleSpec?.title}</h1>
+            <h2 className="mb-8 leading-relaxed">{articleSpec?.price.toLocaleString()} 원</h2>
+            <p className="mb-8 leading-relaxed" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(articleSpec?.content || "") }}></p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">Button</button>
               <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
