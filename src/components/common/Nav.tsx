39737260172e6208fc80c1ef41a3e20b652aa379
khakisage <jongjin201@gmail.com>
2023-07-26 13:39:10 +0900

    Feat: 로그인한 유저 상태 업데이트 - 새로고침 시 유지 안되는 문제 해결 - localstorage에 저장

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -4,18 +4,17 @@ import { loginState, userState } from "../../store/atom";
 import { useNavigate } from "react-router";
 import { auth } from "../../firebase-config";
 import Search from "./Search";
+import { Link } from "react-router-dom";
 
 export default function Nav(): JSX.Element {
   const moveMain = MovePage({ url: "/" });
   const moveSignin = MovePage({ url: "/signin" });
   const navigate = useNavigate();
   const resetUserInfo = useResetRecoilState(userState);
-  // const user = auth.currentUser;
-  console.log("userinfo", useRecoilValue(userState));
-  // const uid = user?.uid;
-  // console.log("uid", uid);
+  const loginUserInfo = JSON.parse(localStorage.getItem("loginUserInfo") as string);
+  const deleteUserInfo = () => localStorage.removeItem("loginUserInfo");
   const [isLogin, setIsLogin] = useRecoilState(loginState);
-  console.log("isLogin", isLogin);
+
   return (
     <div className="navbar bg-first fixed">
       <div className="flex-1">
@@ -30,25 +29,34 @@ export default function Nav(): JSX.Element {
             로그인
           </button>
         ) : (
-          <>
-            <button
-              className="btn bg-third text-fourth hover:bg-third2 border-none"
-              onClick={() => {
-                setIsLogin(false);
-                auth.signOut();
-                resetUserInfo();
-                navigate("/");
-              }}
-            >
-              로그아웃
-            </button>
-            <button className="btn bg-third text-fourth hover:bg-third2 border-none" onClick={() => navigate("/mypage")}>
-              내 정보
-            </button>
-            <button className="btn bg-third text-fourth hover:bg-third2 border-none" onClick={() => navigate("/post")}>
-              글 작성
-            </button>
-          </>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-fullhover:text-bg-fourth">
+                {loginUserInfo ? <img src={loginUserInfo.profileImg} /> : <img src="/Default_profile.png" />}
+              </div>
+            </label>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-first2 rounded-box w-52 ">
+              <li>
+                <Link to={"/mypage"}>내 정보</Link>
+              </li>
+              <li>
+                <Link to={"/post"}>글 작성</Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    setIsLogin(false);
+                    auth.signOut();
+                    resetUserInfo();
+                    navigate("/");
+                    deleteUserInfo();
+                  }}
+                >
+                  로그아웃
+                </button>
+              </li>
+            </ul>
+          </div>
         )}
       </div>
     </div>
