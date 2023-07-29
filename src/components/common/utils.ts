b143a849794b19b88a30e8f2372543a2b749172c
khakisage <jongjin201@gmail.com>
2023-07-30 01:51:10 +0900

    Chore: 불필요 함수 제거

--- src/components/common/utils.ts
+++ src/components/common/utils.ts
@@ -1,6 +1,6 @@
-import { collection, getDocs } from "firebase/firestore";
+// import { collection, getDocs } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
-import { db } from "../../firebase-config";
+// import { db } from "../../firebase-config";
 
 export const MovePage = ({ url }: { url: string }) => {
   const navigate = useNavigate();
@@ -10,23 +10,12 @@ export const MovePage = ({ url }: { url: string }) => {
   return movePage;
 };
 
-type Article = {
-  id: string;
-  uid: string;
-  title: string;
-  content: string;
-  category: string;
-  price: number;
-  image: string | ArrayBuffer | null;
-  // comments: any[];
-};
-
 // fetchArticles 함수는 firebase의 firestore에서 articles 컬렉션의 모든 문서를 가져옵니다.
-export const fetchArticles = async () => {
-  const querySnapshot = await getDocs(collection(db, "articles"));
-  const articles: any = [];
-  querySnapshot.forEach((doc) => {
-    articles.push({ id: doc.id, ...doc.data() });
-  });
-  return articles;
-};
+// export const fetchArticles = async () => {
+//   const querySnapshot = await getDocs(collection(db, "articles"));
+//   const articles: any = [];
+//   querySnapshot.forEach((doc) => {
+//     articles.push({ id: doc.id, ...doc.data() });
+//   });
+//   return articles;
+// };
